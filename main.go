1b3f9ebfdf08a6acc3217376cac02dcd25c7b25d
PragmaOnce <pcumpian1@gmail.com>
2019-07-08 11:49:48 +0200

    added modifiers.go

--- main.go
+++ main.go
@@ -5,27 +5,21 @@ import (
 	"sync"
 )
 
-type stage func(in <-chan int, t transformer) <-chan int
+type (
+	pipe        <-chan int
+	stage       func(in pipe, t transformer) pipe
+	transformer func(int) int
+)
 
-type transformer func(int) int
-
-func sender(outChan chan int, inChan <-chan int, t transformer) {
+func sender(outChan chan int, inChan pipe, t transformer) {
 	for n := range inChan {
 		outChan <- t(n)
 	}
 	close(outChan)
 }
 
-func square(number int) int {
-	return number * number
-}
-
-func add2(number int) int {
-	return number + 2
-}
-
 //the one who emits the data
-func source(numbers ...int) <-chan int {
+func source(numbers ...int) pipe {
 	out := make(chan int)
 	go func() {
 		for _, n := range numbers {
@@ -37,7 +31,7 @@ func source(numbers ...int) <-chan int {
 }
 
 //end this receives all the data and presents it the way you want
-func end(in <-chan int) []int {
+func end(in pipe) []int {
 	out := make([]int, 0)
 
 	var wg sync.WaitGroup
@@ -56,19 +50,19 @@ func end(in <-chan int) []int {
 	return out
 }
 
-func firstStage(in <-chan int, t transformer) <-chan int {
+func firstStage(in pipe, t transformer) pipe {
 	out := make(chan int)
 	go sender(out, in, t)
 	return out
 }
 
-func secondStage(in <-chan int, t transformer) <-chan int {
+func secondStage(in pipe, t transformer) pipe {
 	out := make(chan int)
 	go sender(out, in, t)
 	return out
 }
 
-func thirdStage(in <-chan int, t transformer) <-chan int {
+func thirdStage(in pipe, t transformer) pipe {
 	out := make(chan int)
 	go sender(out, in, t)
 	return out
